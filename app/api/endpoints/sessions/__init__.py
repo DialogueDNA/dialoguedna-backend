8a038fbaebeb110485c9d01c6fce3693ff194d54
Yaniv Zamir <yanivza@mta.ac.il>
2025-06-04 00:15:11 +0300

    Fixed a little bit in the controller and deleted unwanted files and folders

--- app/api/endpoints/sessions/__init__.py
+++ app/api/endpoints/sessions/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from . import download_file_from_azure_blob_storage
 from .metadata import router as metadata_router
 from .upload import router as upload_router
 from .transcript import router as transcript_router
 from .emotions import router as emotions_router
 from .summary import router as summary_router
+from .audio import router as audio_router
+from .delete import router as delete_router
 
 router = APIRouter()
 
@@ -14,6 +15,5 @@ router.include_router(upload_router, prefix="/api/sessions/upload", tags=["uploa
 router.include_router(transcript_router, prefix="/api/sessions/transcript", tags=["transcript"])
 router.include_router(emotions_router, prefix="/api/sessions/emotions", tags=["emotions"])
 router.include_router(summary_router, prefix="/api/sessions/summary", tags=["summary"])
-
-#----------------------------FOR YANIV FROM YARDEN---------------------------------#
-router.include_router(download_file_from_azure_blob_storage.router, prefix="/api/download", tags=["download"])
+router.include_router(audio_router, prefix="/api/sessions/audio", tags=["audio"])
+router.include_router(delete_router, prefix="/api/sessions/delete", tags=["delete"])
